a65892ff275569cbb8b63f080449999bb0c2c8a1
Munna Hossain <mayinuddin.munna@gmail.com>
2022-06-01 16:31:23 +0600

    include search bar

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Text from '../Components/text/text';
 import PlanetHeader from '../Components/PlanetHeader';
 import { colors } from '../theme/colors';
 import { spacing } from '../theme/spacing';
 import { PLANET_LIST } from '../data/PlanetList';
 import { AntDesign } from '@expo/vector-icons';
-import { View, StyleSheet, FlatList, SafeAreaView, Pressable } from 'react-native';
+import { View, StyleSheet, FlatList, SafeAreaView, Pressable, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-const PlanetItem = ({item}) => {
+const PlanetItem = ({ item }) => {
   const { name, color } = item;
   const navigation = useNavigation();
   return (
     <Pressable onPress={() => {
       navigation.navigate("Details", { planet: item });
     }}
-
       style={styles.item}>
       <View style={{ flexDirection: 'row', alignItems: 'center' }}>
         <View style={[styles.circle, { backgroundColor: color }]}></View>
@@ -29,7 +28,7 @@ const PlanetItem = ({item}) => {
 }
 
 export default function Home({ navigation }) {
-
+  const [ list, setList ] = useState(PLANET_LIST);
   const renderItem = ({ item }) => {
     const { name, color } = item;
     return (
@@ -37,15 +36,32 @@ export default function Home({ navigation }) {
     );
   }
 
+  const searchFilter = (text) => {
+    const filterList = PLANET_LIST.filter((item) => {
+      const itemName = item.name.toLowerCase();
+      const userTypeText = text.toLowerCase();
+      return itemName.indexOf(userTypeText) > -1;
+    });
+    setList(filterList);
+  }
+
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <PlanetHeader />
+        <TextInput
+          style={styles.searchInput}
+          placeholderTextColor={colors.white}
+          autoCorrect={false}
+          onChangeText={(text) => searchFilter(text)}
+          placeholder="Type the planet name"
+          keyboardType="text"
+        />
       </View>
       <View>
         <FlatList
           contentContainerStyle={styles.list}
-          data={PLANET_LIST}
+          data={list}
           keyExtractor={(item) => { item.name }}
           renderItem={renderItem}
           ItemSeparatorComponent={() => <View style={styles.separator} />}
@@ -83,11 +99,18 @@ const styles = StyleSheet.create({
   textName: {
     alignItems: 'center',
     textTransform: "uppercase",
-
   },
 
   separator: {
     borderWidth: .3,
     borderBottomColor: colors.white,
+  },
+
+  searchInput: {
+    borderWidth: 1,
+    color: colors.white,
+    marginTop: spacing[6],
+    marginHorizontal: spacing[6],
+    borderBottomColor: colors.white,
   }
-})
+});
